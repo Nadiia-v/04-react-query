b097d36481b5912f2b696d63bb969acd8ba7779f
Nadiia-v <nadiia.kovalenko.vt@gmail.com>
2025-06-08 21:09:55 +0200

    Do the task

---
+++ src/components/App/App.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieModal from "../MovieModal/MovieModal";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+import toast, { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import ReactPaginate from "react-paginate";
+import css from "./App.module.css";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["movies", query, page],
+    queryFn: () => fetchMovies(query, page),
+    enabled: query.trim().length > 0,
+    placeholderData: (previousData) => previousData,
+  });
+  const handleSearch = (newQuery: string) => {
+    setQuery(newQuery);
+    setPage(1);
+  };
+
+  useEffect(() => {
+    if (data && data.results.length === 0 && !isLoading && !isError) {
+      toast("❌ No movies found for your request.");
+    }
+  }, [data, isLoading, isError]);
+
+  return (
+    <>
+      <SearchBar onSubmit={handleSearch} />
+      <Toaster position="top-center" />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+
+      {data && data.results.length > 0 && (
+        <>
+          {data && data.total_pages > 1 && (
+            <ReactPaginate
+              pageCount={data.total_pages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => setPage(selected + 1)}
+              forcePage={Math.max(0, page - 1)}
+              containerClassName={css.pagination}
+              pageClassName={css.li}
+              pageLinkClassName={css.a}
+              activeClassName={css.active}
+              previousLabel="←"
+              nextLabel="→"
+            />
+          )}
+          <MovieGrid movies={data.results} onSelect={setSelectedMovie} />
+        </>
+      )}
+
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+    </>
+  );
+}
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import App from "../src/components/App/App";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
